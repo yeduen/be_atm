036ad2b3c0d6a7ae65482b8d8f995d41b31ebad4
yeduen <gaurga@naver.com>
2025-03-14 13:59:58 +0900

    ✨ 영수증 기능 추가
    사용자가 영수증 내역을 확일할 수 있도록 기능을 추가, 영수증 내역이 없을
    경우 메시지를 출력

--- atm.py
+++ atm.py
@@ -3,6 +3,12 @@
 #숫자로 원하는 기능을 입력할 수 있게 만들어주세요ㅕ. 긜고 사용자가 입력한 기능은 num 변수에 담아주세요
 # deposit_amount: 
 # withdraw_amount:
+# 영수증 기능에 사용되는 데이터타입은 list => ["입금", 5000, 8000], ["출금", 3000, 5000,]
+
+
+#영수증 기능 구현을 위한 빈 리스트 선언
+receipts = []
+
 
 #원금
 balance = 3000
@@ -15,6 +21,9 @@ while True:
         if deposit_amount.isdigit() and int(deposit_amount) > 0: #첫번째 조건 문자가 입력된건 아닌지 확인 / 0보다 큰 금액을 입력했는지
             balance += int(deposit_amount) #balance = balance + ini(deposit_amount)
             print(f"고객님이 입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.")
+
+            #영수증에 데이터 넣기
+            receipts.append(("입금", deposit_amount, balance))
         else:
             print("정신차리고, 제대로된 숫자형태로 입금액을 작성해줘^^ㅗ")
     if num == "2":
@@ -23,8 +32,21 @@ while True:
             withdraw_amount = min(balance, int(withdraw_amount)) # min(8000, 10000) -> 8000원  min(8000, 5000) -> 5000
             balance -= withdraw_amount # balance = balance -= withdraw_amount
             print(f"고객님이 출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.")
+                        
+            #영수증에 데이터 넣기
+            receipts.append(("출금", withdraw_amount, balance))
+        else:
+            print("정신차리고, 제대로된 숫자형태로 입금액을 작성해줘^^ㅗ")
     if num == "3":
-        pass
+        #리스트에 값이 있을때와 없을때 달랐으면 한다.
+        #값이 있을때 출력 결과가 출금 : 3000원 | 잔액 : 50000원과 같이 나왔으면 한다.
+        # " "
+        if receipts:
+            print("=========영수증========")
+            for i in receipts:
+                print(f"{i[0]} : {i[1]}원 | 잔액 {i[2]}")
+        else:
+            print("영수증에 내역이 없습니다.")
     if num == "4":
         print("서비스를 종료합니다.")
         break
