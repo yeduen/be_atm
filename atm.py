72fd27fc9e11b8cba94fc55699cebbb69fe54aea
yeduen <gaurga@naver.com>
2025-03-14 13:34:03 +0900

    ✨ 출금 기능 추가
    사용자가 출금 금액 직접 입력, 잔액에서차감되는 기능 구현, 잔액보다 큰금액 출금 시도시 잔액만큼만 출금되도록 구현

--- atm.py
+++ atm.py
@@ -2,7 +2,9 @@
 #1.입금, 2.출금, 3.영수증 보기, 4.종료 => 글자를 입력 받을지(입금, 출금 ...) / 숫자를 입력받을지(1,2,3..)
 #숫자로 원하는 기능을 입력할 수 있게 만들어주세요ㅕ. 긜고 사용자가 입력한 기능은 num 변수에 담아주세요
 # deposit_amount: 
+# withdraw_amount:
 
+#원금
 balance = 3000
 
 while True:
@@ -16,7 +18,11 @@ while True:
         else:
             print("정신차리고, 제대로된 숫자형태로 입금액을 작성해줘^^ㅗ")
     if num == "2":
-        pass
+        withdraw_amount = input("출금할 금액을 입력해주세요 :") 
+        if withdraw_amount.isdigit() and int(withdraw_amount) > 0:
+            withdraw_amount = min(balance, int(withdraw_amount)) # min(8000, 10000) -> 8000원  min(8000, 5000) -> 5000
+            balance -= withdraw_amount # balance = balance -= withdraw_amount
+            print(f"고객님이 출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.")
     if num == "3":
         pass
     if num == "4":
